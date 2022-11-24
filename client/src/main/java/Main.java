39e71eb3d3556bcd76acee6ff8278c68b4384621
Vladislav <vlad.bel.kos@mail.ru>
2022-11-24 18:50:38 +0300

    Update client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -3,23 +3,21 @@ import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String host = "127.0.0.1";
         int port = 7001;
 
-            try (Socket clientSocket = new Socket(host, port)) {
-                try {
-                    try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-                        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                            out.println("Vladislav");
+        try (Socket clientSocket = new Socket(host, port)) {
+            try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    out.println("Vladislav");
 
-                            String response = in.readLine();
-                            System.out.println(response);
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    String response = in.readLine();
+                    System.out.println(response);
                 }
+            }
+        } catch (IOException e) {
+                    e.printStackTrace();
         }
     }
 }
